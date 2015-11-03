ea1b1f19d908b9ac813d64bd86e8bb1702797492
Ricardo Pérez <ricpelo@gmail.com>
2015-11-03 11:21:05 +0100

    Script de preparación de YouCompleteMe

--- YouCompleteMe.sh
+++ YouCompleteMe.sh
@@ -3,6 +3,7 @@
 sudo apt-get install build-essential cmake python python-dev
 
 cd YouCompleteMe
+git submodule update --init --recursive
 ./install.py --clang-completer --omnisharp-completer --gocode-completer
 cd ..
 
