abed6965d46a0a8e9aacbd67d6740f13d0a93978
Ricardo Pérez <ricpelo@gmail.com>
2017-03-16 19:40:05 +0100

    Retoques en instalación de Ruby con rbenv

--- rbenv-install.sh
+++ rbenv-install.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-sudo apt-get install build-essential libreadline-dev
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' > ~/.oh-my-zsh/custom/rbenv.zsh
 echo 'eval "$(rbenv init -)"' >> ~/.oh-my-zsh/custom/rbenv.zsh
 
+echo "Sal del terminal, vuelve a entrar y ejecuta 'rbenv-postinstall.sh'."
 
--- rbenv-postinstall.sh
+++ rbenv-postinstall.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sudo apt install build-essential libreadline-dev libssl-dev
+
 VER=$(rbenv install -l | cut -c3- | grep "^[0-9.]*$" | sort -r | head -1)
 
 rbenv install $VER
