22179d4ef15d646e2c661f157463c989b5a5a041
Ricardo Pérez <ricpelo@gmail.com>
2017-04-04 13:26:04 +0200

    Instala paquete ricpelo/php-integrator-symbol-viewer de Atom

--- atom/atom-postinstall.sh
+++ atom/atom-postinstall.sh
@@ -2,6 +2,7 @@
 
 echo "Instalando paquetes de Atom..."
 apm install $(<atom-packages.txt)
+apm install ricpelo/php-integrator-symbol-viewer
 if [ ! -f ~/.atom/config.cson ]
 then
     echo "Copiando archivo config.cson en ~/.atom..."
