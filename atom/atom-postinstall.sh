3cb82aea1aa3e67cf3f3e760f59fc219901c07b6
Ricardo Pérez <ricpelo@gmail.com>
2017-06-08 20:32:58 +0200

    Script atom-postinstall.sh reescrito casi por completo

--- atom/atom-postinstall.sh
+++ atom/atom-postinstall.sh
@@ -1,12 +1,29 @@
 #!/bin/sh
 
-echo "Instalando paquetes de Atom..."
-apm install $(<atom-packages.txt)
-apm install ricpelo/php-integrator-symbol-viewer
+L=""
+for p in $(cat atom-packages.txt)
+do
+    if [ ! -d ~/.atom/packages/$p ]
+    then
+        L="$L$p "
+    fi
+done
+if [ ! -z "$L" ]
+then
+    echo "Instalando paquetes de Atom..."
+    apm install $L
+else
+    echo "Todos los paquetes de Atom ya copiados."
+fi
+p="php-integrator-symbol-viewer"
+if [ ! -d ~/.atom/packages/$p ]
+then
+    apm install ricpelo/$p
+fi
 if [ ! -f ~/.atom/config.cson ]
 then
     echo "Copiando archivo config.cson en ~/.atom..."
-    /bin/cp -f config.cson ~/.atom
+    cp -f config.cson ~/.atom
 else
     echo "Archivo config.cson ya copiado en ~/.atom."
 fi
