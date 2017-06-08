111f45e0a63abaf0acda1bcf07966c8da9a955d0
Ricardo Pérez <ricpelo@gmail.com>
2017-06-08 16:16:47 +0200

    Actualiza proyecto.sh

--- proyecto.sh
+++ proyecto.sh
@@ -6,17 +6,20 @@ if [ -z "$1" ]; then
 fi
 
 echo "Creando el proyecto desde la plantilla básica de Yii2..."
-composer create-project yiisoft/yii2-app-basic $1
+composer create-project --no-install --no-scripts yiisoft/yii2-app-basic $1
 echo "Extrayendo el esqueleto modificado del proyecto..."
 curl -L https://github.com/ricpelo/pre/tarball/master | tar xvz --strip 1 -C $1
 cd $1
 echo "Modificando configuración del proyecto..."
-cat web.php | tr '\n' '\f' | sed "s/'log' => .*'db'/'log' => require(__DIR__ . '\/log.php'),\f        'db'/" | tr '\f' '\n' > web.php
+cat config/web.php | tr '\n' '\f' | sed "s/'log' => .*'db'/'log' => require(__DIR__ . '\/log.php'),\f        'db'/" | tr '\f' '\n' > config/web.php.temp
+/bin/mv -f config/web.php.temp config/web.php
 echo "Modificando archivos con el nombre del proyecto..."
 /bin/sed -i s/proyecto/$1/g db/* config/*
 /bin/mv db/proyecto.sql db/$1.sql
-echo "Ejecutando composer update..."
-composer update
+echo "Ejecutando composer..."
+composer install
+composer run-script post-create-project-cmd
+composer install
 echo "Creando repositorio git..."
 git init
 git add .
