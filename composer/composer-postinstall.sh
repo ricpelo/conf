ee5c13e29443cb0d3c32cdd7758cc45dcc121ae4
Ricardo Pérez <ricpelo@gmail.com>
2017-06-28 20:04:37 +0200

    PHP_CodeSniffer 3.0 aún no es compatible con yii2-coding-standards

--- composer/composer-postinstall.sh
+++ composer/composer-postinstall.sh
@@ -8,12 +8,19 @@ then
     echo "Vete a https://github.com/settings/tokens/new?scopes=repo,gist&description=$DESC para crear un token, pulsa en 'Generate token', cópialo y pégalo aquí."
     echo -n "Token: "
     read TOKEN
+    echo "Creando token de GitHub para Composer..."
     composer config -g github-oauth.github.com $TOKEN
+else
+    echo "Token de GitHub ya creado."
 fi
 if [ ! -d /opt/composer ]
 then
+    echo "Creando enlace simbólico de /opt/composer a $COMPOSER_DIR..."
     COMPOSER_DIR=$(composer config -g home)
     sudo ln -sf $COMPOSER_DIR /opt/composer
+else
+    echo "Enlace simbólico /opt/composer ya creado."
 fi
-composer global require --prefer-dist friendsofphp/php-cs-fixer squizlabs/php_codesniffer yiisoft/yii2-coding-standards
+echo "Instalando paquetes globales interesantes para proyectos..."
+composer global require --prefer-dist friendsofphp/php-cs-fixer "squizlabs/php_codesniffer:^2.0" yiisoft/yii2-coding-standards
 
