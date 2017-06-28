0ebc17bf5872e3119dff6d945584243902801184
Ricardo Pérez <ricpelo@gmail.com>
2017-06-28 19:41:00 +0200

    Script composer-install.sh más informativo

--- composer/composer-install.sh
+++ composer/composer-install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "Descargando y ejecutando instalador de composer..."
+
 EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
@@ -14,6 +16,7 @@ fi
 php composer-setup.php --quiet
 RESULT=$?
 rm composer-setup.php
+echo "Instalando en /usr/local/bin/composer..."
 sudo install -o root -g root composer.phar /usr/local/bin/composer
 rm composer.phar
 if [ "`sudo cat /etc/sudoers | tail -c1`" != ""  ]
