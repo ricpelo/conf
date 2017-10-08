7ba233700815b364b78c975da000d76fae565e4d
Ricardo Pérez <ricpelo@gmail.com>
2017-10-08 12:09:59 +0200

    Instala exactamente la versión 7.1 de PHP

--- scripts/php-install.sh
+++ scripts/php-install.sh
@@ -10,11 +10,11 @@ else
 fi
 
 echo "Instalando paquetes ensenciales de PHP..."
-P="php php7.1 libapache2-mod-php php-cli"
+P="php7.1 libapache2-mod-php7.1 php7.1-cli"
 echo "\033[1;32m\$\033[0m\033[35m sudo apt -y install $P\033[0m"
 sudo apt -y install $P
 echo "Instalando paquetes adicionales..."
-P="php-pgsql php-sqlite3 sqlite php-intl php-mbstring php-gd php-curl php-xml php-xdebug php-json"
+P="php7.1-pgsql php7.1-sqlite3 sqlite sqlite3 php7.1-intl php7.1-mbstring php7.1-gd php7.1-curl php7.1-xml php7.1-xdebug php7.1-json"
 echo "\033[1;32m\$\033[0m\033[35m sudo apt -y install $P\033[0m"
 sudo apt -y install $P
 
