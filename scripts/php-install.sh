5a2d525278f95045ddfbf689173e3be940a184de
Ricardo Pérez <ricpelo@gmail.com>
2017-10-23 20:17:54 +0200

    Instala Apache2 junto con PHP y activa el módulo de PHP en php-install.sh

--- scripts/php-install.sh
+++ scripts/php-install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+. $(dirname $(readlink -f "$0"))/_lib/auxiliar.sh
+
 if [ ! -f /etc/apt/sources.list.d/ondrej-ubuntu-php-$(lsb_release -sc).list ]
 then
     echo "Activando el repositorio de PHP..."
@@ -10,13 +12,21 @@ else
 fi
 
 echo "Instalando paquetes ensenciales de PHP..."
-P="php7.1 libapache2-mod-php7.1 php7.1-cli"
+P="php7.1 apache2 libapache2-mod-php7.1 php7.1-cli"
 echo "\033[1;32m\$\033[0m\033[35m sudo apt -y install $P\033[0m"
 sudo apt -y install $P
 echo "Instalando paquetes adicionales..."
 P="php7.1-pgsql php7.1-sqlite3 sqlite sqlite3 php7.1-intl php7.1-mbstring php7.1-gd php7.1-curl php7.1-xml php7.1-xdebug php7.1-json"
 echo "\033[1;32m\$\033[0m\033[35m sudo apt -y install $P\033[0m"
 sudo apt -y install $P
+
+if ! a2query -q -m php7.1
+then
+    echo "Activando módulo de PHP para Apache2..."
+    sudo a2enmod php7.1
+else
+    echo "Módulo de PHP para Apache2 ya activado."
+fi
 
 desactiva_xdebug
 
