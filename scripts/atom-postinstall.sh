6ad9b634e454b414cd70db609196aa520ddec2df
Ricardo Pérez <ricpelo@gmail.com>
2017-12-13 10:49:54 +0100

    Pregunta si eliminar configuración de Atom y desinstala paquetes sobrantes

--- scripts/atom-postinstall.sh
+++ scripts/atom-postinstall.sh
@@ -7,10 +7,35 @@ comprueba_atom
 comprueba_php
 comprueba_composer
 
+CONF=$HOME/.atom
+
+if [ -d "$CONF" ]
+then
+    echo "Se ha detectado una configuración previa de Atom en $CONF."
+    echo -n "¿Eliminarla previamente para una instalación limpia? (S/n): "
+    read SN
+    [ "$SN" = "s" ] && SN="S"
+    if [ "$SN" = "S" ]
+    then
+        echo "Eliminando directorio $CONF..."
+        rm -rf $CONF
+    else
+        QUITAR=$(apm list --installed --bare | cut -d"@" -f1 | diff - $BASE_DIR/atom/atom-packages.txt | grep "^< " | cut -c3-)
+        if [ -n "$QUITAR" ]
+        then
+            echo "Desinstalando paquetes sobrantes..."
+            for p in $(echo $QUITAR)
+            do
+                apm uninstall $p
+            done
+        fi
+    fi
+fi
+
 P=""
 for p in $(cat $BASE_DIR/atom/atom-packages.txt)
 do
-    if [ ! -d ~/.atom/packages/$p ]
+    if [ ! -d "$CONF/packages/$p" ]
     then
         P="$P$p "
     fi
@@ -22,10 +47,10 @@ then
 else
     echo "Todos los paquetes de Atom ya instalados."
 fi
-echo "Copiando archivos de configuración en ~/.atom..."
+echo "Copiando archivos de configuración en $CONF..."
 for f in keymap.cson config.cson; do
-    [ -f ~/.atom/$f ] && mv -f ~/.atom/$f ~/.atom/$f.viejo
-    cp -f $BASE_DIR/atom/$f ~/.atom
+    [ -f "$CONF/$f" ] && mv -f "$CONF/$f" "$CONF/$f.viejo"
+    cp -f $BASE_DIR/atom/$f $CONF
 done
 asegura_salto_linea_sudoers
 desactiva_sudo "/usr/bin/apm"
