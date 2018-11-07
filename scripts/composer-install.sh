dbaa36c1c56897d2fe95b15f42474b686ebe5633
Ricardo Pérez <ricpelo@gmail.com>
2018-11-07 15:49:04 +0100

    Corrige instalador de Composer

--- scripts/composer-install.sh
+++ scripts/composer-install.sh
@@ -8,11 +8,11 @@ comprueba_php $CALLA
 
 mensaje "Descargando y ejecutando instalador de composer..."
 
-EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
+EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
 
-if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE"  ]; then
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]; then
     >&2 echo 'ERROR: Invalid installer signature'
     rm composer-setup.php
     exit 1
