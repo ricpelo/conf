27835001efcc6cbb3755c96d89516d2dd31a6e22
Ricardo Pérez <ricpelo@gmail.com>
2019-03-28 09:01:32 +0100

    Integra composer-postinstall.sh en composer-install.sh

--- scripts/composer-install.sh
+++ scripts/composer-install.sh
@@ -31,4 +31,37 @@ if dpkg -s composer > /dev/null 2>&1; then
     sudo apt -y purge composer
 fi
 
+while true; do
+    TOKEN=$(token_composer)
+    if [ -z "$TOKEN" ]; then
+        TOKEN=$(github token)
+        if [ -n "$TOKEN" ]; then
+            mensaje "Creando token de GitHub para Composer..."
+            token_composer $TOKEN
+            break
+        else
+            mensaje "El token para GitHub no está definido aún."
+            mensaje "Ejecuta el script git-config.sh antes de continuar."
+            pregunta SN "¿Quieres hacerlo ahora?" N $CALLA
+            if [ "$SN" = "S" ]; then
+                $BASE_DIR/git-config.sh
+            else
+                mensaje_error "Imposible continuar. Vuelve cuando hayas ejecutado el script git-config.sh"
+                exit 1
+            fi
+        fi
+    else
+        GITHUB=$(github token)
+        if [ "$TOKEN" != "$GITHUB" ]; then
+            echo "Actualizando token de Composer para que coincida con el de GitHub..."
+            token_composer $GITHUB
+            break
+        fi
+        mensaje "Token de GitHub para Composer ya creado."
+        break
+    fi
+done
+
+desactiva_xdebug
+
 exit $RESULT
